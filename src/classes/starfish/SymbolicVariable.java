e9e6ed4c3dccfe3efbedcf59f72db434091a5ff7
Quoc-Sang Phan <dark2bright@gmail.com>
2017-10-16 16:32:53 -0700

    add customized domains

---
+++ src/classes/starfish/SymbolicVariable.java
@@ -0,0 +1,6 @@
+package starfish;
+
+public class SymbolicVariable {
+
+	native public static void setMinMax(int var, int min, int max);
+}
---
+++ src/peers/starfish/JPF_starfish_SymbolicVariable.java
@@ -0,0 +1,19 @@
+package starfish;
+
+import gov.nasa.jpf.annotation.MJI;
+import gov.nasa.jpf.symbc.numeric.IntegerExpression;
+import gov.nasa.jpf.vm.MJIEnv;
+import gov.nasa.jpf.vm.NativePeer;
+import starfish.variable.GlobalVariables;
+
+public class JPF_starfish_SymbolicVariable extends NativePeer {
+
+	@MJI
+	public static void setMinMax__III__V(MJIEnv env, int objRef, int var, int min, int max){
+		Object [] attrs = env.getArgAttributes();
+		IntegerExpression sym_arg = (IntegerExpression)attrs[0];
+		if (sym_arg !=null){
+			GlobalVariables.domains.put(attrs[0].toString(), new starfish.variable.Domain(min,max));
+		}
+	}
+}
